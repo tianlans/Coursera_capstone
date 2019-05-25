bd4b05482f6d8239b0cabe885ef192b4a9a39f44
tianlans <51020308+tianlans@users.noreply.github.com>
2019-05-25 22:09:56 +0800

    Add files via upload

---
+++ week 1 project.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# This notebook is used for capstone project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
